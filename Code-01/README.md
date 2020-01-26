f7c0bb5c1347d868c9fa4f37d19332c356ad886b
Heinz Ramos <heinzramos@gmail.com>
2020-01-26 02:18:25 -0600

    Initial commit

---
+++ Code-01/README.md
@@ -0,0 +1,39 @@
+# Assumptions
+1. No monitoring required
+2. No logging required
+3. No code deployment required, default landing page is ok
+4. Performance is not required, free tier can be used
+5. No Terraform state management required, local state is ok
+5. Basic Terraform knowledge
+6. Azure knowledge
+
+# Requirements
+1. Azure Subscription
+2. Account with at least Contributor access to the Azure Subscription
+3. Azure CLI or Azure Cloud Shell
+    - Installation guide: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli
+    - Or, use chocolatey to install: choco install azure-cli
+4. Terraform 0.12.20+
+    - Can be downloaded from: https://www.terraform.io/downloads.html
+    - Or, use chocolatey to install: choco install terraform
+    - Available on Azure Cloud Shell (Bash or Powershell)
+5. Internet Connection
+    - Preferrably run from a system that doesn't require a proxy to go out the internet
+    - If proxy is required, configure "netsh winttp set proxy" or proxy environment variables
+    - AzureRM provider can also be used with Azure Service Principal by uncommenting and populating the block (line 34-37)
+
+# Steps
+1. Login to Azure using Azure CLI or through Azure Cloud Shell
+    - Command: az login
+2. If there are multiple subscription, set the account
+    - Command: az account set -s [Subscription Name or ID]
+3. Change directory to where the main.tf is extracted/downloaded
+4. Run Terraform Plan
+    - Review the plan, and confirm values are ok
+    - Variables can be bypassed using: var = 'foo=bar' (can be set multiple times)
+5. Run Terraform Apply
+    - Type "yes" to proceed
+6. There will be an output for website_url, site should be accessible within seconds, browse away!
+7. Run Terraform Destroy to delete the deployed resources
+    - Ensure this is ran on the same directory during plan/apply (state file will be located there)
+    - Type "yes" to proceed
---
+++ Code-01/main.tf
@@ -0,0 +1,88 @@
+# ------------------------------------------------------------
+#  Variables
+# ------------------------------------------------------------
+variable "rg_name" {
+  default     = "RG-Website"
+  description = "Name of the resource group"
+}
+variable "rg_location" {
+  default     = "East US 2"
+  description = "Location of resource group"
+}
+variable "environment" {
+  default = "demo"
+}
+
+locals {
+  # Hash of Tags
+  common_tags = {
+    "Deployment Method" = "Terraform"
+    "Code Author"       = "Heinz Ramos"
+    "Environment"       = var.environment
+  }
+}
+
+# ------------------------------------------------------------
+#  Main Process
+# ------------------------------------------------------------
+# Azure Connection, assume connection is made using Azure CLI
+provider "azurerm" {
+  /* 
+    Only required if Azure CLI authentication is having issues
+    This will use an Azure Service Principal with contributor rights to the sbuscription
+
+    subscription_id = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    client_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    client_secret   = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    tenant_id       = "XXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  */
+}
+
+# Create the Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.rg_name}-${var.environment}"
+  location = var.rg_location
+  tags     = local.common_tags
+}
+
+# Create App Service Plan
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "ASP-Website-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.common_tags
+
+  sku {
+    tier = "Free"
+    size = "F1"
+  }
+}
+
+# Create a Random String
+resource "random_string" "app_service" {
+  length  = 4
+  special = false
+}
+
+# Create App Service
+resource "azurerm_app_service" "app_service" {
+  name                = "AS-Website-${random_string.app_service.result}-${var.environment}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  https_only          = "true"
+  tags                = local.common_tags
+}
+
+# ------------------------------------------------------------
+#  Outputs
+# ------------------------------------------------------------
+output "website_url" {
+  value = "https://${azurerm_app_service.app_service.default_site_hostname}"
+}
+
+output "website_xcredentials" {
+  sensitive = true
+  value     = azurerm_app_service.app_service.site_credential
+}
+
